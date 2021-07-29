b6bf328444ac7a8710ae45f9c63f4677135ec428
vladsemyr <vladsemyr@mail.ru>
2021-07-29 10:23:05 +0300

    Отправка текста и вырисовка его в центральной области

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -58,11 +58,15 @@ namespace ComConsole
         private void DarkModeToggleButton_Checked(object sender, RoutedEventArgs e)
         {
             ToggleBaseColour(true);
+            if (ConsoleCurrentLineBorder != null)
+                ConsoleCurrentLineBorder.Background = new SolidColorBrush(Color.FromArgb(0x15, 0xff, 0xff, 0xff));
         }
 
         private void DarkModeToggleButton_Unchecked(object sender, RoutedEventArgs e)
         {
             ToggleBaseColour(false);
+            if (ConsoleCurrentLineBorder != null)
+                ConsoleCurrentLineBorder.Background = new SolidColorBrush(Color.FromArgb(0x15, 0x00, 0x00, 0x00));
         }
 
 
@@ -85,6 +89,16 @@ namespace ComConsole
                 Menu.Padding = new Thickness(10, 10, 0, 10);
                 MenuLeftRightBorder.CornerRadius = new CornerRadius(20, 0, 0, 20);
                 MenuArrow.Text = "<";
+            }
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                ConsoleLog.Text = ConsoleLog.Text + ConsoleCurrentLine.Text + "\n";
+                ConsoleCurrentLine.Text = "";
+                ConsoleScroll.ScrollToEnd();
             }
         }
     }
